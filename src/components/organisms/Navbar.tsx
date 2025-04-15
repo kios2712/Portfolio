ae61fc779a3f1834ef9747852926ede651bcc1da
Kiosma Rincones <43353796+kios2712@users.noreply.github.com>
2025-04-15 15:32:03 -0500

    inicio de creacion de componentes

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,20 +1,29 @@
-
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-     <div>
-      <section></section>
-      <nav className="bg-black ">
-        <ul className="flex justify-center gap-6 p-4 text-white">
-          <li><Link href="/">About</Link></li>
-          <li><Link href="/">Projects</Link></li>
-          <li><Link href="/">Skill</Link></li>
-          <li><Link href="/">Contact</Link></li>
+    <div className="flex justify-center gap-1 mt-10 bg-white">
+      <section className="w-20 p-2 text-sm text-white rounded-sm bg-black/80">
+        Kiosma <br /> Rincones
+      </section>
+      <nav className="w-full rounded-sm shadow-lg bg-black/80">
+        <ul className="flex justify-center gap-10 p-4 text-sm text-white ">
+          <li className="border-b-2 border-black">
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/skills">Skills</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
-      <section></section>
-     </div>
-      );
-    
+      <section className="w-20 p-2 text-sm rounded-sm bg-black/80">
+      </section>
+    </div>
+  );
 }
